84f5202cfad895e1343a3e776cc6dd293c046417
manuelfdng-insight <Kokko.Ng@insight.com>
2025-03-27 14:32:46 +0800

    modified gitingest file

--- ingest.sh
+++ ingest.sh
@@ -1,1 +1,19 @@
-gitingest . -e "__pycache__/" -e "*.pyc" -e "*.pyo" -e "*.pyd" -e ".venv/" -e "venv/" -e "test_*.py" -e "*_test.py" -e "tests/" -e "testing/" -e ".pytest_cache/" -e "htmlcov/" -e ".coverage" -e "coverage.xml" -e ".tox/" -e "*.md"
+gitingest . \
+  -e "__pycache__/" \
+  -e "*.pyc" \
+  -e "*.pyo" \
+  -e "*.pyd" \
+  -e ".venv/" \
+  -e "venv/" \
+  -e "test_*.py" \
+  -e "*_test.py" \
+  -e "tests/" \
+  -e "testing/" \
+  -e ".pytest_cache/" \
+  -e "htmlcov/" \
+  -e ".coverage" \
+  -e "coverage.xml" \
+  -e ".tox/" \
+  -e "*.md" \
+  -e "*.png" \
+  -e "*.ico"
