998f248474dce8a11f5573e34d0f0d41cc528256
manuelfdng-insight <Kokko.Ng@insight.com>
2025-03-31 10:44:47 +0800

    removed init_db()

--- celery_worker.py
+++ celery_worker.py
@@ -15,10 +15,6 @@ flask_app = create_app(env)
 # Get Celery app from Flask app
 celery = flask_app.celery
 
-# Ensure database session is initialized
-# This explicit import is important to initialize models
-init_db(flask_app)
-
 # Explicitly import task modules to ensure tasks are registered
 import app.tasks.transcription_tasks
 import app.tasks.upload_tasks
