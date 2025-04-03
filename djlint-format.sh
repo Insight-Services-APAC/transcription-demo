70260fd0f38a8283b9aee20c8efc82378fcf82ea
manuelfdng-insight <Kokko.Ng@insight.com>
2025-04-03 18:57:03 +0800

    applied djlint

---
+++ djlint-format.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Build a list of pruned paths from .gitignore
+PRUNE_ARGS=()
+while IFS= read -r line; do
+  [[ -z "$line" || "$line" =~ ^# ]] && continue
+  if [[ "$line" == */ ]]; then
+    DIR="./${line%/}"
+    PRUNE_ARGS+=(-path "$DIR" -prune -o)
+  fi
+done < .gitignore
+
+# Find all HTML/Jinja files not in ignored dirs and format them with djlint
+find . "${PRUNE_ARGS[@]}" \( -name "*.html" -o -name "*.jinja" -o -name "*.jinja2" \) -print | xargs djlint --reformat --profile=jinja
